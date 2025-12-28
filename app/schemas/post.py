109aa931caedc9e50ed1af14519e596ba56d1ce6
lazyjsh03 <lazy.jsh03@gmail.com>
2025-12-29 01:12:28 +0900

    feat: auth, post dto 작성

---
+++ app/schemas/post.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+
+
+class PostResponse(PostBase):
+    id: int
+    author_id: int
